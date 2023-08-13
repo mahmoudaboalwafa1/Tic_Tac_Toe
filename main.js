624fa0d4a10756a9d45bb4078711e6cddbf87d7d
Mahmoud Abo Alwafa <112mahmoudsayed@gmail.com>
2023-08-13 07:20:44 +0200

    ADD sass & start Game area

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // Global Variables
-let result = document.querySelector(".result");
-let boxes = document.querySelectorAll(".box");
-let app = document.querySelector(".app");
+const game_container = document.querySelector(".game-container");
+const startGameArea = document.querySelector(".start-game");
+const result = document.querySelector(".result");
+const boxes = document.querySelectorAll(".box");
+const app = document.querySelector(".app");
+const name = document.getElementById("name");
+const x = document.getElementById("x");
+const o = document.getElementById("o");
+const btnStart = document.querySelector(".start-game button");
 
 let play = "x";
 
-function endGame(num1, num2, num3) {
+btnStart.addEventListener("click", () => HandleBTNStart());
+
+const endGame = (num1, num2, num3) => {
   num1.style.backgroundColor = "black";
   num2.style.backgroundColor = "black";
   num3.style.backgroundColor = "black";
@@ -13,9 +21,9 @@ function endGame(num1, num2, num3) {
   result.innerHTML = `${num1.innerHTML.toUpperCase()} Is Winner`;
   setInterval(() => (result.innerHTML += "."), 1000);
   setTimeout(() => window.location.reload(), 4000);
-}
+};
 
-function static() {
+const static = () => {
   boxes.forEach((box, index) => {
     box.onclick = () => {
       if (box.innerHTML == "" && play == "x") {
@@ -91,6 +99,20 @@ function static() {
       }
     };
   });
-}
+};
 
+const HideGame = (status) => {
+  game_container.style.display = status;
+};
+
+const startGame = () => {
+  name?.value?.length === 0 && HideGame("none");
+};
+
+const HandleBTNStart = () => {
+  name?.value?.length > 0 && HideGame("block");
+  startGameArea.style.display = "none";
+};
+
+startGame();
 static();
